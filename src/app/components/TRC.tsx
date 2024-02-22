5067617b16357ff3ce1a446c5afd7e29fb6e4322
ManuBhardwaj001 <100307788+ManuBhardwaj001@users.noreply.github.com>
2024-02-22 19:31:57 +0530

    added hero text reveal card

---
+++ src/app/components/TRC.tsx
@@ -0,0 +1,23 @@
+"use client";
+import React from "react";
+import {
+  TextRevealCard,
+  TextRevealCardDescription,
+  TextRevealCardTitle,
+} from "./ui/text-reveal-card";
+
+export function TRC() {
+  return (
+    <div className="flex">
+      <TextRevealCard text="Manu Bhardwaj" revealText="Frontend Developer ">
+        <TextRevealCardTitle>
+          Sometimes, you just need to see it.
+        </TextRevealCardTitle>
+        <TextRevealCardDescription>
+          This is a text reveal card. Hover over the card to reveal the hidden
+          text.
+        </TextRevealCardDescription>
+      </TextRevealCard>
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 import Head from "next/head";
-import { BsFillMoonStarsFill } from 'react-icons/bs';
+import { BsFillMoonStarsFill } from "react-icons/bs";
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 import Image from "next/image";
 import Avatar from "../../public/Avatar.png";
@@ -8,42 +8,49 @@ import html from "../../public/tech_stack/html.png";
 import css from "../../public/tech_stack/css.png";
 import react from "../../public/tech_stack/react.png";
 import next from "../../public/tech_stack/next.png";
-import java from "../../public/tech_stack/java.png"
-import javascript from "../../public/tech_stack/javascript.png"
-import bootstrap from "../../public/tech_stack/bootstrap.png"
-import tailwind from "../../public/tech_stack/tailwind.png"
-import git from "../../public/tech_stack/git.png"
+import java from "../../public/tech_stack/java.png";
+import javascript from "../../public/tech_stack/javascript.png";
+import bootstrap from "../../public/tech_stack/bootstrap.png";
+import tailwind from "../../public/tech_stack/tailwind.png";
+import git from "../../public/tech_stack/git.png";
 import { useState } from "react";
 import { TailwindcssButtons } from "./components/TailwindcssButtons";
 import { TextGenerateEffectDemo } from "./components/TGE";
-import { TextRevealCard } from "./components/ui/text-reveal-card";
 import { SparklesPreview } from "./components/SparklesPreview";
 import { ThreeDCardDemo } from "./components/ThreeDNetflix";
 import { ThreeDSavvySquare } from "./components/ThreeDSavvySquare";
-
-
+import { TRC } from "./components/TRC";
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
   return (
-
     <div className={darkMode ? "dark" : ""}>
-      <Head>
-
-      </Head>
+      <Head></Head>
       <main className="bg-white px-10 dark:bg-gray-900 dark:text-white">
         <section className="min-h-screen">
           <nav className="py-5 mb-12 flex justify-between">
-            <h1 className="text-xl font-burtons cursor-default">Developed by Manu</h1>
+            <h1 className="text-xl font-burtons cursor-default">
+              Developed by Manu
+            </h1>
             <ul className="flex items-center">
-              <li><BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className="cursor-pointer text-2xl" /></li>
+              <li>
+                <BsFillMoonStarsFill
+                  onClick={() => setDarkMode(!darkMode)}
+                  className="cursor-pointer text-2xl"
+                />
+              </li>
               {/* <li>className="bg-gradient-to-t from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="#">Resume</a></li> */}
-              <li><a href="./Resume.pdf" download="Resume.pdf" ><TailwindcssButtons /></a></li>
-
+              <li>
+                <a href="./Resume.pdf" download="Resume.pdf">
+                  <TailwindcssButtons />
+                </a>
+              </li>
             </ul>
           </nav>
-          <SparklesPreview />
-          {/* <TextRevealCard /> */}
+          <div className="flex justify-center">
+            <TRC />
+          </div>
+          {/* <SparklesPreview /> */}
           <div className="text-center p-10">
             {/* <h2 className="text-5xl py-2 text-teal-600 font-medium">Manu Bhardwaj</h2>
             <h3 className="text-2xl py-2">Frontend Developer</h3> */}
@@ -54,13 +61,20 @@ export default function Home() {
             <a target="_blank" href="https://github.com/ManuBhardwaj001">
               <AiFillGithub className="cursor-pointer"></AiFillGithub>
             </a>
-            <a target="_blank" href="https://www.linkedin.com/in/manu-bhardwaj-467290282/">
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/in/manu-bhardwaj-467290282/"
+            >
               <AiFillLinkedin />
             </a>
-
           </div>
           <div className="relative mx-auto background-transparent w-80 h-80 mt-20 rounded-full overflow-hidden">
-            <Image className=" h-80 rounded-full" alt="Avatar" src={Avatar} fill />
+            <Image
+              className=" h-80 rounded-full"
+              alt="Avatar"
+              src={Avatar}
+              fill
+            />
           </div>
         </section>
         <section>
@@ -72,19 +86,61 @@ export default function Home() {
           <div className="flex shadow-lg py-10 rounded-xl flex-col items-center mt-10 space-y-4 dark:shadow-white">
             {/* Top row */}
             <div className="flex space-x-4">
-              <Image className="cursor-pointer transition-transform duration-500 transform hover:rotate-180" alt="react" src={react} width={100} height={100} />
+              <Image
+                className="cursor-pointer transition-transform duration-500 transform hover:rotate-180"
+                alt="react"
+                src={react}
+                width={100}
+                height={100}
+              />
             </div>
             {/* Middle row */}
             <div className="flex space-x-4">
-              <Image className="cursor-pointer transition-transform duration-1000 transform hover:scale-x-[-1]" alt="next" src={next} width={100} height={100} />
-              <Image className="cursor-pointer transition-transform duration-1000 transform hover:scale-x-[-1]" alt="bootstrap" src={bootstrap} width={100} height={100} />
-              <Image className="cursor-pointer transition-transform duration-1000 transform hover:scale-x-[-1]" alt="html" src={html} width={100} height={100} />
+              <Image
+                className="cursor-pointer transition-transform duration-1000 transform hover:scale-x-[-1]"
+                alt="next"
+                src={next}
+                width={100}
+                height={100}
+              />
+              <Image
+                className="cursor-pointer transition-transform duration-1000 transform hover:scale-x-[-1]"
+                alt="bootstrap"
+                src={bootstrap}
+                width={100}
+                height={100}
+              />
+              <Image
+                className="cursor-pointer transition-transform duration-1000 transform hover:scale-x-[-1]"
+                alt="html"
+                src={html}
+                width={100}
+                height={100}
+              />
             </div>
             {/* Bottom row */}
             <div className="flex space-x-4">
-              <Image className="cursor-pointer transition-transform duration-1000 transform hover:scale-x-[-1]" alt="javascript" src={javascript} width={100} height={100} />
-              <Image className="cursor-pointer transition-transform duration-1000 transform hover:scale-x-[-1]" alt="java" src={java} width={100} height={100} />
-              <Image className="cursor-pointer transition-transform duration-1000 transform hover:scale-x-[-1]" alt="git" src={git} width={100} height={100} />
+              <Image
+                className="cursor-pointer transition-transform duration-1000 transform hover:scale-x-[-1]"
+                alt="javascript"
+                src={javascript}
+                width={100}
+                height={100}
+              />
+              <Image
+                className="cursor-pointer transition-transform duration-1000 transform hover:scale-x-[-1]"
+                alt="java"
+                src={java}
+                width={100}
+                height={100}
+              />
+              <Image
+                className="cursor-pointer transition-transform duration-1000 transform hover:scale-x-[-1]"
+                alt="git"
+                src={git}
+                width={100}
+                height={100}
+              />
             </div>
           </div>
         </section>
@@ -97,9 +153,7 @@ export default function Home() {
             <ThreeDSavvySquare />
           </div>
         </section>
-
       </main>
     </div>
   );
 }
-
